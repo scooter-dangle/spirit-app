5614356d8e1a5fd2bd435729c453adeb3ef8f098
Scott Steele <scottlsteele@gmail.com>
2014-06-19 04:41:39 -0400

    Add failing delete spec for domains of deleted account

--- test/integration/deleting_accounts_test.rb
+++ test/integration/deleting_accounts_test.rb
@@ -7,4 +7,12 @@ class DeletingAccountsTest < ActionDispatch::IntegrationTest
     delete "/accounts/#{@account.id}"
     assert_equal 204, response.status
   end
+
+  test 'domains owned by account are deleted with account' do
+    domain = Domain.create! account_id: @account.id, hostname: 'theonion.com'
+    delete "/accounts/#{@account.id}"
+
+    get "domains/#{domain.id}"
+    assert_equal 404, response.status
+  end
 end
