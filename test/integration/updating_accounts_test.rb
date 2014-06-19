ba54d0b6b0eabe3275a512ccce4abae9cc36de65
Scott Steele <scottlsteele@gmail.com>
2014-06-19 03:43:20 -0400

    Add failing update test with nil name

--- test/integration/updating_accounts_test.rb
+++ test/integration/updating_accounts_test.rb
@@ -3,12 +3,20 @@ require 'test_helper.rb'
 class UpdatingAccountsTest < ActionDispatch::IntegrationTest
   setup { @account = Account.create! name: 'Kosh' }
 
-  test 'update' do
+  test 'update success' do
     patch "/accounts/#{@account.id}",
       { account: { name: 'Kosh aka Raggedy Puppy' } }.to_json,
       { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
 
     assert response.success?
     assert_equal 'Kosh aka Raggedy Puppy', @account.reload.name
+  end
+
+  test 'update fail with empty name' do
+    patch "/accounts/#{@account.id}",
+      { account: { name: nil } }.to_json,
+      { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
+
+    assert_equal 422, response.status
   end
 end
