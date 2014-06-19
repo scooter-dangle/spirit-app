128692d5c98629ea443a60db28c38aa640a845c7
Scott Steele <scottlsteele@gmail.com>
2014-06-19 02:48:04 -0400

    Add account show, create, and validation

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,28 @@
 class AccountsController < ApplicationController
   def index
     accounts = Account.all
+    if (name = params[:name])
+      accounts = accounts.where name: name
+    end
     render json: accounts, response: 200
+  end
+
+  def show
+    account = Account.find params[:id]
+    render json: account, response: 200
+  end
+
+  def create
+    account = Account.new account_params
+    if account.save
+      render json: account, status: 201, location: account
+    else
+      render json: account.errors, status: 422
+    end
+  end
+
+  private
+  def account_params
+    params.require(:account).permit :name
   end
 end
