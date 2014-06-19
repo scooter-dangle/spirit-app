acf9b15c38fe9b62f9261963885b934560d939a9
Scott Steele <scottlsteele@gmail.com>
2014-06-19 07:57:15 -0400

    Set up calls to delayed_job method

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -1,3 +1,5 @@
+require 'resolv'
+
 class DomainsController < ApplicationController
   def index
     domains = Domain.all
@@ -21,6 +23,7 @@ class DomainsController < ApplicationController
   def create
     domain = Domain.new domain_params.merge({ip_address: 'n/a'})
     if domain.save
+      ip_lookup domain.id, hostname: domain_params[:hostname]
       head 201, location: domain
     else
       render json: domain.errors, status: 422
@@ -29,7 +32,12 @@ class DomainsController < ApplicationController
 
   def update
     domain = Domain.find params[:id]
-    if domain.update domain_params.merge({ip_address: 'n/a'})
+    full_params = domain_params
+    full_params.merge!({ip_address: 'n/a'}) unless domain.hostname == full_params[:hostname]
+    if domain.update full_params
+      if full_params[:ip_address] == 'n/a'
+        ip_lookup domain.id, hostname: (domain_params[:hostname] || domain.hostname)
+      end
       head 200
     else
       render json: domain.errors, status: 422
@@ -46,5 +54,15 @@ class DomainsController < ApplicationController
   def domain_params
     params.require(:domain).permit :account_id, :hostname
   end
+
+  def ip_lookup domain_id, hostname
+    begin
+      ip_address = Resolv.getaddress hostname
+    rescue Resolv::ResolvError
+      ip_address = 'invalid'
+    end
+    Domain.update domain_id, ip_address: ip_address
+  end
+  handle_asynchronously :ip_lookup
 end
 
