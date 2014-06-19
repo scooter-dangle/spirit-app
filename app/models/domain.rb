7686ca3bf10c4e572dca6f606dbc4254ad9428c0
Scott Steele <scottlsteele@gmail.com>
2014-06-19 07:28:26 -0400

    Validate presence of account_id in domain

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -2,4 +2,7 @@ class Domain < ActiveRecord::Base
   validates :hostname,   presence: true, uniqueness: true
   validates :ip_address, presence: true
   belongs_to :account
+  # There may be pitfalls to this belongs_to and validates presence...
+  # Possibly see http://www.samuelmullen.com/2013/12/validating-presence-of-associations-and-foreign-keys-in-rails/
+  validates :account_id, presence: true
 end
